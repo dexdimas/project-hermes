e8efa91b082d3b61c42b508c284572c3a4a85c48
Andimas Muhammad Bagaswara <andimas.bagaswara@gmail.com>
2020-07-25 17:23:22 +0800

    Initial Commit

---
+++ project-hermes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-hermes
+//
+//  Created by Andimas Bagaswara on 25/07/20.
+//  Copyright © 2020 Andimas Bagaswara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
